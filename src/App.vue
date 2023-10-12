ab01d978da2d348c91838a88cc0e09e5b8323181
Manu199 <manu199gh@gmail.com>
2023-10-12 16:47:10 +0200

    chiamata api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <script>
+
+import axios from 'axios';
+import Header from './components/Header.vue'
+import { store } from './data/store'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  },
+  data(){
+    return{
+      store
+      
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl)
+      .then( res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(errorrrrr);
+      })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
 
 
 <template>
-  
-<h1>Yu ghi </h1>
+  <Header titleStr="Yu Gi Oh"/>
 
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'Header',
+    props: {
+        titleStr: String
+    }
+}
+</script>
+
+
+<template>
+  <div class="logo">
+    <!-- <img src="../../public/Yugioh_anime_logo.webp" alt="" -->
+    <h1>{{ titleStr }}</h1>
+  </div>
+</template>
+
+
+
+<style>
+
+</style>
